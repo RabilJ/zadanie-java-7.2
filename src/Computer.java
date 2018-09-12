036a63ae7d48d27f9ef424cc0429c1c84317450c
RabilJ <jakub.popielarski7@gmail.com>
2018-09-12 17:44:47 +0200

    Zadania 1.2

--- src/Computer.java
+++ src/Computer.java
@@ -3,4 +3,15 @@ public class Computer {
     String processor;
     String graphicCard;
     int memory;
+
+    public Computer(String proc, String graph, int mem){
+        processor = proc;
+        graphicCard = graph;
+        memory = mem;
+    }
+
+    @Override
+    public String toString() {
+        return processor+' '+graphicCard+' '+memory+"GB";
+    }
 }
--- src/Shop.java
+++ src/Shop.java
@@ -2,19 +2,14 @@ public class Shop {
     public static void main(String[] args) {
 
 
-        Computer comp1 = new Computer();
-        Computer comp2 = new Computer();
+        Computer comp1 = new Computer("Intel Core i5","Nvidia",16);
+        Computer comp2 = new Computer("Intel Core i7","MSI",32);
 
-        comp1.processor = "Intel Core i5";
-        comp1.graphicCard = "Nvidia";
-        comp1.memory = 16;
+        System.out.println(comp1);
+        System.out.println(comp2);
 
-        comp2.processor = "Intel Core i7";
-        comp2.graphicCard = "Zotac";
-        comp2.memory = 32;
 
-        System.out.println("Comp1: "+comp1.processor+" "+comp1.graphicCard+" "+comp1.memory+"GB");
-        System.out.println("Comp2: "+comp2.processor+" "+comp2.graphicCard+" "+comp2.memory+"GB");
+
 
     }
 }
