7ef0cf29afb248e2b8f2ead5e26a03166c6d4ec6
RabilJ <jakub.popielarski7@gmail.com>
2018-09-13 17:35:16 +0200

    Zadania 1.3

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    //marka, model, rocznik, pojemność silnika
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+
+    public Car(String br, String mod, int yr, double cap){
+        brand = br;
+        model = mod;
+        year = yr;
+        capacity = cap;
+    }
+}
