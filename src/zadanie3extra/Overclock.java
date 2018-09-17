2e40a35d066b4d2e48c8e01b5fcf77be7acc78c8
RabilJ <jakub.popielarski7@gmail.com>
2018-09-17 19:59:08 +0200

    Zadanie 3extra

---
+++ src/zadanie3extra/Overclock.java
@@ -0,0 +1,10 @@
+package zadanie3extra;
+
+public class Overclock {
+    public void increaseCpuClock(Computer comp, double speedUp) {
+        comp.procesor.timing = comp.procesor.timing + speedUp;
+    }
+    public void decreaseCpuClock(Computer comp, double slowDown) {
+        comp.procesor.timing = comp.procesor.timing - slowDown;
+    }
+}
