35cbcf6061125fa582e56175d79f7135a6014947
RabilJ <jakub.popielarski7@gmail.com>
2018-09-26 19:34:28 +0200

    Zajęcia nr 6, zadanie 6.1

---
+++ src/zadania6/Task1.java
@@ -0,0 +1,18 @@
+package zadania6;
+
+import java.util.Random;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(10);
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+" ");
+            System.out.print(numbers[(numbers.length-1)-i]+" ");
+        }
+    }
+}
