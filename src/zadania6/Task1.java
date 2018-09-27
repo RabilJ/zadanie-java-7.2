48f49985a61e1b566cb7966df3eecb797b8de304
RabilJ <jakub.popielarski7@gmail.com>
2018-09-27 22:59:19 +0200

    Zajecia nr 6, Zadanie 6.1 poprawka

--- src/zadania6/Task1.java
+++ src/zadania6/Task1.java
@@ -1,5 +1,7 @@
 package zadania6;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task1 {
@@ -10,9 +12,13 @@ public class Task1 {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rand.nextInt(10);
         }
+        System.out.println(Arrays.toString(numbers));
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i]+" ");
-            System.out.print(numbers[(numbers.length-1)-i]+" ");
+            System.out.print(numbers[i] + " ");
+            if (i == (numbers.length - 1)) {
+                for (int j = 0; j < numbers.length; j++)
+                    System.out.print(numbers[(numbers.length - 1) - j] + " ");
+            }
         }
     }
 }
