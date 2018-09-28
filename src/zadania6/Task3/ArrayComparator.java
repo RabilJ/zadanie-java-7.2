4b313f11954ced7c986a332e22c67ffb6c29a69a
RabilJ <jakub.popielarski7@gmail.com>
2018-09-28 12:37:18 +0200

    Zajecia nr 6, Zadanie 6.3 poprawka

--- src/zadania6/Task3/ArrayComparator.java
+++ src/zadania6/Task3/ArrayComparator.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class ArrayComparator {
     public boolean compare(int[] numbers, int[] numbers2) {
+        if (numbers.length!=numbers2.length){
+            return false;
+        }
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers.length!=numbers2.length||numbers[i] > numbers2[i]) {
-                return false;
-            } else if (numbers.length!=numbers2.length||numbers[i] < numbers2[i]) {
+            if (numbers[i]!=numbers2[i]) {
                 return false;
             }
         }
