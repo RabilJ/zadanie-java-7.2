87aa6d97f91b804ef6c78321dda3483f35c91545
RabilJ <jakub.popielarski7@gmail.com>
2018-09-30 13:39:17 +0200

    Zajecia nr 6, Zadanie 6.1 poprawka

---
+++ src/zadanie7/Pierwsze/CalendarConverter.java
@@ -0,0 +1,33 @@
+package zadanie7.Pierwsze;
+
+public class CalendarConverter {
+    public static final int MONDAY = 1;
+    public static final int TUESDAY = 2;
+    public static final int WEDNESDAY= 3;
+    public static final int THURSDAY = 4;
+    public static final int FRIDAY = 5;
+    public static final int SATURDAY = 6;
+    public static final int SUNDAY = 7;
+
+public static String dayToString(int dayNumber){
+        switch (dayNumber){
+            case MONDAY:
+                return "Poniedziałek";
+            case TUESDAY:
+                return "Wtorek";
+            case WEDNESDAY:
+                return "Środa";
+            case THURSDAY:
+                return "Czwartek";
+            case FRIDAY:
+                return "Piątek";
+            case SATURDAY:
+                return "Sobota";
+            case SUNDAY:
+                return "Niedziela";
+                default:
+                    return "Wybierz liczbę reprezentującą dzień tygodnia";
+
+    }
+}
+}
---
+++ src/zadanie7/Pierwsze/ConverterTest.java
@@ -0,0 +1,10 @@
+package zadanie7.Pierwsze;
+
+public class ConverterTest {
+    public static void main(String[] args) {
+        for (int i = 1; i <=7 ; i++) {
+            System.out.println(i+" dzień tygodnia to "+CalendarConverter.dayToString(i));
+
+        }
+    }
+}
